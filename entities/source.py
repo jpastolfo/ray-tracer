6cee7f8f6c51fd1a64a161b41b6416457149adfd
João Astolfo <joao.astolfo@lnls.br>
2023-09-16 16:24:22 -0300

    16/09/2023 new source Lamp

--- entities/source.py
+++ entities/source.py
@@ -4,13 +4,25 @@ import numpy as np
 from .beam import Ray
 
 class Source:
-    def __init__(self,x:float,y:float,angle:float,color:str):
+    def __init__(self,x:float,y:float,angle:float,color:str,
+                 n_rays:int=1):
         self.x = x
         self.y = y
         self.angle = angle
         self.color = color
-    
+        self.n_rays = n_rays
+
     
     def generate_ray(self):
         ray = Ray(self.x,self.y,self.angle,self.color)
         return ray
+    
+
+class Lamp(Source):
+    def __init__(self,x:float,y:float,color:str,
+                 n_rays:int=1):
+        super().__init__(x,y,0,color,n_rays)
+        
+    def generate_rays(self):
+        rays = [Ray(self.x,self.y,angle,self.color) for angle in np.linspace(0,360,self.n_rays)]
+        return rays
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import sys,os
 
 import matplotlib.pyplot as plt
 from entities.beam import Ray
-from entities.source import Source
+from entities.source import *
 from entities.optical_elements import PlaneMirror
 
 sys.path.append("/home/astolfo/Projects/ray-tracer/test.py")
@@ -10,18 +10,18 @@ sys.path.append("/home/astolfo/Projects/ray-tracer/test.py")
 
 plt.figure()
 
-source = Source(-5,0,0,"red")
+#source = Source(-5,0,0,"red")
+lamp = Lamp(-5,0,"red",100)
 mirror0 = PlaneMirror(0,0,40,4)
 mirror1 = PlaneMirror(0,5,-135,6)
 mirror2 = PlaneMirror(5,5,135,10)
 mirror3 = PlaneMirror(5,0,-45,4)
-mirror4 = PlaneMirror(8,0,90,4)
 
-objects = [mirror0,mirror1,mirror2,mirror3,mirror4]
+objects = [mirror0,mirror1,mirror2,mirror3]
 
-ray = source.generate_ray()
-ray.trace(objects)
-ray.draw()
+for ray in lamp.generate_rays():
+    ray.trace(objects)
+    ray.draw()
 
 
 plt.xlim(-10,10)
